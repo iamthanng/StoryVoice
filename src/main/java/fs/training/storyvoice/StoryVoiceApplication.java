616a9184d6cc725a3a643dbae893b8a01adf90ab
iamthanng <iamthanng@gmail.com>
2026-08-10 09:43:29 +0700

    Initial commit

---
+++ src/main/java/fs/training/storyvoice/StoryVoiceApplication.java
@@ -0,0 +1,13 @@
+package fs.training.storyvoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryVoiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryVoiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fs/training/storyvoice/StoryVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fs.training.storyvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryVoiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
